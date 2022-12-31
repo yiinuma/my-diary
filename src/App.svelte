27c9ecda5c7e3367711a8cd7e8813894d6881139
yiinuma <65111001+yiinuma@users.noreply.github.com>
2022-12-31 14:39:08 +0900

    init

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import './app.css'
+</script>
+
+<h1 class="grid h-screen place-content-center text-8xl">
+  Heading
+</h1>
+<button
+  class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
+>
+  ...
+</button>
+<button
+  class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800"
+>
+  ...
+</button>
