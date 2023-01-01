aa8410cbe867c84b7a00bc7d6eb1e9ed7473150b
yiinuma <65111001+yiinuma@users.noreply.github.com>
2023-01-01 11:30:51 +0900

    add firebase init

--- src/App.svelte
+++ src/App.svelte
@@ -8,13 +8,12 @@
   import Create from './components/Create.svelte'
   import Diary from './components/Diary.svelte'
   import About from './components/About.svelte'
+  import './helpers/firebase'
 
   export let url
 </script>
 
-<main
-  class="text-center max-w-full mx-auto bg-background md:max-w-none"
->
+<main class="text-center max-w-full mx-auto bg-background md:max-w-none">
   <Header />
   <section class="min-h-[calc(100vh-96px)]">
     <Router {url}>
